087920036660d86fe5332a647d783458bde6f006
jddelia <jddelia123@gmail.com>
2023-03-14 15:28:45 -0400

    update

---
+++ howSum.js
@@ -0,0 +1,21 @@
+// This function shows the list of numbers which sum up to a given target value.
+
+function howSum(target, nums) {
+  if (target === 0) return [];
+  if (target < 0) return false;
+
+  for (let i = 0; i < nums.length; i++) {
+    let result = howSum(target - nums[i], nums);
+
+    if (result) {
+      return [nums[i], ...result];
+    }
+  }
+
+  return false;
+}
+
+console.log(howSum(7, [2, 3, 4]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(200, [6, 9]));
---
+++ howSumMemoized.js
@@ -0,0 +1,24 @@
+// This function shows the list of numbers which sum up to a given target value.
+
+function howSum(target, nums, memo = {}) {
+  if (target in memo) return memo[target];
+  if (target === 0) return [];
+  if (target < 0) return false;
+
+  for (let i = 0; i < nums.length; i++) {
+    let result = howSum(target - nums[i], nums, memo);
+
+    if (result) {
+      memo[target] = [nums[i], ...result];
+      return memo[target];
+    }
+  }
+
+  memo[target] = false;
+  return memo[target];
+}
+
+console.log(howSum(7, [2, 3, 4]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(2000, [6, 9]));
