7b68d3cd559ab23e0ed9130d7280d999dccf5a07
jddelia <jddelia123@gmail.com>
2023-03-14 22:13:00 -0400

    update

--- bestSumMemoized.js
+++ bestSumMemoized.js
@@ -1,25 +1,27 @@
 // This function returns the shortest list of numbers which sum up to a given target value.
 
-function bestSum(target, nums) {
+function bestSum(target, nums, memo = {}) {
+  if (target in memo) return memo[target];
   if (target === 0) return [];
   if (target < 0) return false;
 
   let shortestList = null;
   for (let i = 0; i < nums.length; i++) {
-    let result = bestSum(target - nums[i], nums);
+    let result = bestSum(target - nums[i], nums, memo);
 
     if (result) {
-      let newList = [nums[i], ...result];
-      if (!shortestList || newList.length < shortestList.length) {
-        shortestList = newList;
+      memo[target] = [nums[i], ...result];
+      if (!shortestList || memo[target].length < shortestList.length) {
+        shortestList = memo[target];
       }
     }
   }
 
+  memo[target] = false;
   return shortestList;
 }
 
 console.log(bestSum(7, [2, 3, 4]));
 console.log(bestSum(7, [2, 4]));
 console.log(bestSum(8, [2, 3, 5]));
-console.log(bestSum(200, [6, 9]));
+console.log(bestSum(2000, [6, 9]));
